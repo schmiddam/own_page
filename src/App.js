e0f1bb181dedebbd89acbe9ed5cefc4e3bb3d8cd
schmiddam <damaris.schmid2@uzh.ch>
2021-01-15 13:26:31 +0100

    Zwei Typewriterlinien mit versetztem Start II

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 // import Letterize from "letterizejs";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <img src={pic} className="Daemr-bg" alt="daemr" />
-          <h3 className="Typewriter-title">
-              I am Damaris Schmid.
-          </h3>
-          <a className="Typewriter-cs">
-              Computer Science Student.
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={pic} className="Daemr-bg" alt="daemr" />
+                <h3 className="Typewriter-title">
+                    I am Damaris Schmid.
+                </h3>
+                <a className="Typewriter-cs">
+                    Computer Science Student.
 
-          </a>
-      </header>
-    </div>
-  );
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
